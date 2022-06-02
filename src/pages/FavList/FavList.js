a0288704111814bd4fe6c01260d4d3fc7a5e27af
DamianDAW <damian.lopez@viewnext.com>
2022-06-02 10:22:45 +0100

    v2.6.1 market mobile

---
+++ src/pages/FavList/FavList.js
@@ -0,0 +1,41 @@
+import { useContext } from "react"
+import { useNavigate } from "react-router-dom";
+import FavContext from "../../context/FavContext";
+import { MobileList } from "../_components/MobileList/MobileList"
+import './FavList.css'
+
+export const FavList = () => {
+
+  const {favorites } = useContext(FavContext)
+ 
+  const navigate = useNavigate()   
+  const handleReturn = () => {
+     navigate(-1)
+  }     
+
+
+  if(favorites.length === 0 ) {
+    return (      
+      <div className='center'>
+        <h4>No items added to favorites</h4>
+        <button className='button' onClick={handleReturn}>
+                Return
+        </button>
+      </div>            
+    ) 
+  }
+    return (
+      <div>     
+        <div>
+        <h4>Favorites</h4>
+          <hr/>
+        </div> 
+        <MobileList mobiles={favorites} />
+        <div className="button-div">
+          <button className='button' onClick={handleReturn}>
+                  Return
+          </button>
+        </div>
+      </div>  
+    )
+}
---
+++ src/pages/_components/MobileList/MobileList.js
@@ -0,0 +1,18 @@
+import { MobileCard } from "../MobileCard/MobileCard"
+import "./MobileList.css"
+
+export const MobileList = ({ mobiles }) => {
+
+  return (
+    <ul className="mobilesGrid  animate__animated animate__fadeIn">
+      {
+        // Optional Chaining ?.
+        mobiles?.map((mobile) => (
+          <li key={ mobile.id }>
+            <MobileCard mobile={mobile} />
+          </li>
+        ))           
+      }       
+    </ul>    
+  )
+}
