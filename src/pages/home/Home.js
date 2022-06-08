262383a441fbfd0c3b31f77a390fc4c0c1a3b09d
DamianDAW <damian.lopez@viewnext.com>
2022-06-08 16:57:49 +0100

    v3.0 market mobile

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { MobileList } from "../_components/MobileList/MobileList";
 import { getMobiles } from '../../services/getMobiles'
 import { FiSearch } from 'react-icons/fi'
 import './Home.css'
+import AppContext from "../../context/AppContext";
+import { Link, useNavigate } from "react-router-dom";
 
 
 export const Home = () => { 
    
   const [ filterSearch, setfilterSearch ] = useState('');
   const [ mobiles, setMobiles ] = useState([])
+  const{userData} = useContext(AppContext)
 
+  const navigate = useNavigate()   
+  const handleClickHome = () => {
+    return navigate('/mobiles/favorites')
+  }
 
 
   const getFilteredMobile = () => {
@@ -35,9 +42,7 @@ export const Home = () => {
   return (
     <div>
       <div className="search-div">
-        {/* <h4>Filter</h4>
-        <hr/> */}
-        <div className="search-box">
+         <div className="search-box">
           <input
             type="text"
             placeholder="Filter search..."  
@@ -61,6 +66,39 @@ export const Home = () => {
             <span>The item searched doesn't exists</span>                 
           )    
         }
+         <div className="footer-basic">
+          <footer>          
+              {
+                userData.isLogged ?
+                <ul className="list-inline">
+                  <li className="list-inline-item">
+                  <Link                  
+                    to="/mobiles"
+                    >
+                    <span>Home</span>
+                  </Link>
+                  </li>
+                  <li className="list-inline-item">
+                    <Link                  
+                      to="/mobiles/favorites"
+                      >
+                      <span>Favorites</span>
+                    </Link>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="##">Cart</a>
+                  </li>                
+                </ul>
+                :
+                <ul className="list-inline">
+                  <li className="list-inline-item"><a href={handleClickHome}>Home</a></li>
+                  <li className="list-inline-item"><a href="##">Terms</a></li>
+                  <li className="list-inline-item"><a href="##">Privacy Policy</a></li>
+                </ul>
+              }
+              <p className="copyright">Market Mobile © 2022</p>
+          </footer>
+        </div>
       </div>  
   )
   
