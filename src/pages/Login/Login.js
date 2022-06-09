9fce0e9335a2d7ac96d8c8c400ddcee5d21d5244
silviademiguel7 <silviademiguel7@gmail.com>
2022-06-09 09:29:27 +0200

    add validation form in login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AppContext from "../../context/AppContext";
 import "./Login.css";
@@ -7,14 +7,28 @@ export const Login = () => {
   const navigate = useNavigate();
   const { userInfo, setUserInfo } = useContext(AppContext);
   const [password, setPassWord] = useState("");
+  const [email, setEmail] = useState("");
+  const [isEmailInputVisited, setIsEmailInputVisited] = useState(false);
+  const [isPasswordInputVisited, setIsPasswordInputVisited] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorPassWordMessage, setErrorPasswordMessage] = useState("");
+  const [errorEmailMessage, setErrorEmailMessage] = useState("");
 
-  const handleChange = (event) => {
+  useEffect(() => {
+    validateInput(password, "password");
+    validateInput(email, "email");
+  }, []);
+
+  const handleChangeEmail = (event) => {
+    validateInput(event.target.value, "email");
+    setEmail(event.target.value);
     setUserInfo({ ...userInfo, email: event.target.value });
   };
   const handlePassword = (event) => {
+    validateInput(event.target.value, "password");
     setPassWord(event.target.value);
   };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     setIsLoading(true);
@@ -25,6 +39,33 @@ export const Login = () => {
     setUserInfo({ ...userInfo, isLogged: true });
   };
 
+  const validateInput = (inputValue, inputType) => {
+    if (inputType === "password") {
+      if (inputValue === "") {
+        setErrorPasswordMessage("Debes indicar un passowrd");
+      } else {
+        const isOnlyNumbers = /^[0-9]+$/;
+
+        setErrorPasswordMessage(
+          isOnlyNumbers.test(inputValue) ? "" : "Formato Password incorrecto"
+        );
+      }
+    }
+    if (inputType === "email") {
+      if (inputValue === "") {
+        setErrorEmailMessage("Debes indicar un email");
+      } else {
+        const isValidEmail =
+          /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        setErrorEmailMessage(
+          isValidEmail.test(inputValue) ? "" : "No es un email valido"
+        );
+      }
+    }
+    return;
+  };
+
   return (
     <div className="login-container">
       {isLoading ? (
@@ -33,26 +74,42 @@ export const Login = () => {
         <form onSubmit={handleSubmit}>
           <div className="form-item">
             <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              name="email"
-              id="email"
-              value={userInfo.email ?? ""}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-item">
-            <label htmlFor="password">Password</label>
-            <input
-              type="text"
-              name="password"
-              id="password"
-              value={password}
-              onChange={handlePassword}
-            />
+            <div className="input-container">
+              <input
+                type="text"
+                name="email"
+                id="email"
+                value={email}
+                onChange={handleChangeEmail}
+                onBlur={() => setIsEmailInputVisited(true)}
+              />
+              {errorEmailMessage && isEmailInputVisited && (
+                <div className="error-message">{errorEmailMessage}</div>
+              )}
+            </div>
           </div>
 
-          <input type="submit" value="Submit" />
+          <div className="form-item">
+            <label htmlFor="password">Password</label>
+            <div className="input-container">
+              <input
+                name="password"
+                id="password"
+                value={password}
+                onChange={handlePassword}
+                onBlur={() => setIsPasswordInputVisited(true)}
+              />
+              {errorPassWordMessage && isPasswordInputVisited && (
+                <div className="error-message">{errorPassWordMessage}</div>
+              )}
+            </div>
+          </div>
+
+          <input
+            type="submit"
+            value="Submit"
+            disabled={errorEmailMessage || errorPassWordMessage}
+          />
         </form>
       )}
     </div>
